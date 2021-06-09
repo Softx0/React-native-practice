dfc3241ab9ef1fa57c96d56dde44414d940eca32
Emmanuel Valenzuela <enmanuel54sk8@gmail.com>
2021-06-09 19:28:57 -0400

    FUNCIONA el UserList XD

--- screens/UserList.js
+++ screens/UserList.js
@@ -1,53 +1,49 @@
 import React, { useState, useEffect } from 'react'
-import { ScrollView, Button } from "react-native";
+import { Button } from "react-native";
 import firebase from "../database/firebase";
 import { ListItem, Avatar } from "react-native-elements";
+import { ScrollView } from "react-native-gesture-handler";
 
 const UserList = (props) => {
-
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        return firebase.db.collection('users').onSnapshot(querySnapshot => {
-
-            const users = [];
-            querySnapshot.docs.forEach(doc => {
+        firebase.db.collection("users").onSnapshot((querySnapshot) => {
+            const usersArray = [];
+            querySnapshot.docs.forEach((doc) => {
                 const { name, email, phone } = doc.data();
-                users.push({
+                usersArray.push({
                     id: doc.id,
                     name,
                     email,
                     phone
                 });
             });
-            setUsers(users);
+            setUsers(usersArray);
         });
-    }, [])
+    }, []);
 
     return (
-        <>
-            <ScrollView>
-                <Button
-                    title="Create User"
-                    onPress={() => props.navigation.navigate('CreateUser')}
-                />
-
-                {
-                    users.map(user => {
-                        console.log(user.name);
-                        <ListItem key={user.id} >
-                            <Avatar rounded source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg' }} />
+        <ScrollView>
+            <Button
+                onPress={() => props.navigation.navigate('CreateUser')}
+                title="Create User"
+            />
+            {
+                users.map((user) => {
+                    return (
+                        <ListItem key={user.id} bottomDivider >
+                            <Avatar title={user.name.charAt(0).toUpperCase()} rounded source={{ uri: 'https://thispersondoesnotexist.com/image' }} />
                             <ListItem.Content>
                                 <ListItem.Title>{user.name}</ListItem.Title>
                                 <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
                             </ListItem.Content>
                             <ListItem.Chevron />
                         </ListItem>
-                    })
-                }
-
-            </ScrollView>
-        </>
+                    )
+                })
+            }
+        </ScrollView>
     )
 }
 
